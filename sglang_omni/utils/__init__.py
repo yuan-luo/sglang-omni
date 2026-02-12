5426ca8cf8953e8140736c1fcf0b0377c1e641fe
FrankLeeeee <somerlee.9@gmail.com>
2026-02-12 01:56:50 +0000

    polish

--- sglang_omni/utils/__init__.py
+++ sglang_omni/utils/__init__.py
@@ -1,2 +1,8 @@
-from .hf import *  # noqa: F403
-from .misc import *  # noqa: F403
+from .hf import instantiate_module, load_hf_config
+from .misc import import_string
+
+__all__ = [
+    "load_hf_config",
+    "instantiate_module",
+    "import_string",
+]
